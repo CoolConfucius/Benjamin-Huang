b76e517d7097ebb9b3ba78f049ee61ca32937302
Benjamin Huang <coolconfucius@yahoo.com>
2016-02-24 15:29:15 -0800

    separated home and resume. Implemented bootstrap navbar

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = angular.module('myApp', ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {url: '/', templateUrl: './html/home.html' })
+    .state('resume', {url: '/resume', templateUrl: './html/resume.html' })
   
   $urlRouterProvider.otherwise('/'); 
 });
